40769589213ac8a77938d609e8078c0872e3016b
Xavier Routh <xavierrouth@gmail.com>
2022-11-04 00:40:05 +0000

    xav first commit :)

---
+++ code/src/pgn_to_fen.py
@@ -0,0 +1,18 @@
+import chess
+import sys
+
+def main(fen: str, outFile: str):
+    board = chess.Board(fen)
+    moves = list(board.legal_moves)
+    f = open(outFile, 'w')
+
+    for move in moves:
+        f.write(str(move) + "\n")
+    return
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
+
+
