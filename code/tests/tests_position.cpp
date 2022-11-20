1decbd164eae587ba5e288a2808373c0b58377e4
Xavier Routh <xavierrouth@gmail.com>
2022-11-20 21:25:11 +0000

    added basic test for reading position from file

--- code/tests/tests_position.cpp
+++ code/tests/tests_position.cpp
@@ -2,15 +2,36 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "src/position.h"
+#include "position.h"
+#include "utils.h"
 
 using namespace std;
 
-
-TEST_CASE("firstTest")
+TEST_CASE("test Position::default")
 {
 	Position pos;
 	Position::Piece p = pos.board[pos.toIndex("a2")];
 	REQUIRE((p.type == Position::Piece::PAWN && p.color == LIGHT));
-	REQUIRE(true);
+	REQUIRE(pos.legal_moves.size() == 0);
+
+}
+
+TEST_CASE("test Position::fromFile")
+{
+	std::ifstream file("../tests/output.txt");
+    std::string line;
+    std::getline(file, line);
+
+    std::vector<std::string> data;
+    SplitString(line, ',', data);
+
+	Position pos(data);
+	Position::Piece N = pos.board[pos.toIndex("e4")];
+	REQUIRE((N.type == Position::Piece::KNIGHT && N.color == LIGHT));
+
+	Position::Piece q = pos.board[pos.toIndex("g6")];
+
+	REQUIRE((q.type == Position::Piece::QUEEN && q.color == DARK));
+	REQUIRE(pos.legal_moves.size() == 32);
+	REQUIRE(pos.evaluation > 130);
 }
