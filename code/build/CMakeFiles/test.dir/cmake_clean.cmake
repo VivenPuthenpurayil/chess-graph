b15ec8e7faf8b00a320712ad4fdf8ff524c40be7
Aman Singh <amans11@illinois.edu>
2022-11-29 07:21:13 +0000

    clean test cases + data cleaning

---
+++ code/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/tests/test_data.cpp.o"
+  "CMakeFiles/test.dir/tests/test_data.cpp.o.d"
+  "CMakeFiles/test.dir/tests/tests_graph.cpp.o"
+  "CMakeFiles/test.dir/tests/tests_graph.cpp.o.d"
+  "CMakeFiles/test.dir/tests/tests_position.cpp.o"
+  "CMakeFiles/test.dir/tests/tests_position.cpp.o.d"
+  "CMakeFiles/test.dir/tests/tests_tarjans.cpp.o"
+  "CMakeFiles/test.dir/tests/tests_tarjans.cpp.o.d"
+  "test"
+  "test.pdb"
+  "test_tests-b12d07c.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/build/src/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/cs225/Chess-graph/code/src/graph.cpp" "src/CMakeFiles/src.dir/graph.cpp.o" "gcc" "src/CMakeFiles/src.dir/graph.cpp.o.d"
+  "/workspaces/cs225/Chess-graph/code/src/graph_algorithms.cpp" "src/CMakeFiles/src.dir/graph_algorithms.cpp.o" "gcc" "src/CMakeFiles/src.dir/graph_algorithms.cpp.o.d"
+  "/workspaces/cs225/Chess-graph/code/src/graph_generator.cpp" "src/CMakeFiles/src.dir/graph_generator.cpp.o" "gcc" "src/CMakeFiles/src.dir/graph_generator.cpp.o.d"
+  "/workspaces/cs225/Chess-graph/code/src/position.cpp" "src/CMakeFiles/src.dir/position.cpp.o" "gcc" "src/CMakeFiles/src.dir/position.cpp.o.d"
+  "/workspaces/cs225/Chess-graph/code/src/utils.cpp" "src/CMakeFiles/src.dir/utils.cpp.o" "gcc" "src/CMakeFiles/src.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
