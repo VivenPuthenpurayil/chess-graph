67133c4d510eed07f491347dd94af02ab5619e91
Xavier Routh <xavierrouth@gmail.com>
2022-11-29 04:13:55 +0000

    added new test

--- code/tests/tests_tarjans.cpp
+++ code/tests/tests_tarjans.cpp
@@ -68,3 +68,72 @@ TEST_CASE("test Tarjans::hard")
     REQUIRE(tarjans(g) == std::vector<int>{0, 0, 0, 3, 3, 5, 5, 5, 8});
 
 }
+
+TEST_CASE("test Tarjans::harder")
+{
+    Graph g(9);
+    g.insertEdge(0, 4);
+    g.insertEdge(4, 0);
+    g.insertEdge(0, 2);
+    g.insertEdge(2, 0);
+    g.insertEdge(2, 3);
+    g.insertEdge(3, 1);
+    g.insertEdge(1, 3);
+    g.insertEdge(5, 4); 
+    g.insertEdge(4, 3); 
+    g.insertEdge(5, 1);
+    g.insertEdge(6, 1);
+    g.insertEdge(5, 6);
+    g.insertEdge(6, 7);
+    g.insertEdge(7, 5);
+    g.insertEdge(8, 8);
+    g.insertEdge(8, 6);
+    g.insertEdge(8, 7);
+
+    REQUIRE(tarjans(g) == std::vector<int>{0, 1, 0, 1, 0, 5, 5, 5, 8});
+}
+// Example for how to retreive useful data from our lowlink list.
+TEST_CASE("test Tarjans::parse lowlink")
+{
+    int s = 0; 
+    int num_scc = 0;
+    float average_size_scc = 0;
+    int max_size_scc = 0;
+
+    Graph g(9);
+
+    // Build our graph (same as test Tarjans::hard)
+    g.insertEdge(0, 4); //
+    g.insertEdge(4, 0); //
+    g.insertEdge(0, 2);
+    g.insertEdge(2, 0);
+    g.insertEdge(2, 3);
+    g.insertEdge(3, 1);
+    g.insertEdge(1, 3);
+    g.insertEdge(5, 4); //
+    g.insertEdge(4, 3); //
+    g.insertEdge(5, 1);
+    g.insertEdge(6, 1);
+    g.insertEdge(5, 6);
+    g.insertEdge(6, 7);
+    g.insertEdge(7, 5);
+    g.insertEdge(8, 8);
+    g.insertEdge(8, 6);
+    g.insertEdge(8, 7);
+
+    std::vector<int> lowlink = tarjans(g);
+
+    s = (int) lowlink.size();
+
+    std::sort(lowlink.begin(), lowlink.end());
+    auto last = std::unique(lowlink.begin(), lowlink.end());
+    lowlink.resize(std::distance(lowlink.begin(), last));
+
+    num_scc = lowlink.size();
+    average_size_scc = (float) s / num_scc;
+
+    REQUIRE(average_size_scc > 1);
+    REQUIRE(num_scc == 4);
+
+
+}
