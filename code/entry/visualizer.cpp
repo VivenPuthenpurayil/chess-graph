71e9f8f7e799d74d8430ea1b5050bfb24ce7df86
Xavier Routh <xavierrouth@gmail.com>
2022-12-04 20:17:43 +0000

    new entrypoint for visualizer

---
+++ code/entry/visualizer.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "utils.h"
+#include "position.h"
+#include "graph.h"
+#include "graph_algorithms.h"
+#include "graph_generator.h"
+
+int main() {
+    std::cout << "test" << "\n";
+}
