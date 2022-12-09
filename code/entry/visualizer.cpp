6a9bb72fc2c5c15958feba1af5e3a0ea01a40d72
Xavier Routh <xavierrouth@gmail.com>
2022-12-09 03:59:02 +0000

    add arg parsers to visualizer

--- code/entry/visualizer.cpp
+++ code/entry/visualizer.cpp
@@ -13,11 +13,47 @@
 #include "graph_generator.h"
 #include "visualizer.h"
 
-int main() {
+/**
+ * Command line options / flags
+ * -n [number] -o [outfolder] -g [attack|support|position] -c [black|white|both]
+*/
+int main(int argc, char **argv) {
     // Read in first line of our test data
+    InputParser input(argc, argv);
+
     std::ifstream file("../../data/preprocessed.txt");
 
-    for (int i = 0; i < 50; i++) {
+    if (!input.cmdOptionExists("-n")) {
+        std::cerr << "Please specify number of positions using -n" << std::endl;
+        return 1;
+    }
+
+    int n = stoi(input.getCmdOption("-n"));
+
+    Image output;
+    std::string outfolder;
+    std::string outfile;
+    
+    if (input.cmdOptionExists("-o")) {
+        outfolder = input.getCmdOption("-o");
+    }
+    else {
+        outfolder = "../output/";
+    }
+    
+    // Default values
+    std::string graph_option = "support";
+    std::string color_option = "white";
+
+    if (input.cmdOptionExists("-g")) {
+        graph_option = input.getCmdOption("-g");
+    }
+
+    if (input.cmdOptionExists("-c")) {
+        color_option = input.getCmdOption("-c");
+    }
+
+    for (int i = 0; i < n; i++) {
         std::string line;
         std::getline(file, line);
 
@@ -25,15 +61,19 @@ int main() {
         SplitString(line, ',', data);
 
         // Build position from data
-        
         Position p(data);
+
         Graph white_support = generateSupport(p, LIGHT);
         Graph black_support = generateSupport(p, DARK);
         Graph union_support = Graph::g_union(white_support, black_support);
+    
         Graph white_attack = generateAttack(p, LIGHT);
         Graph black_attack = generateAttack(p, DARK);
-        Image output;
-        std::string outfile;
+        Graph union_attack = Graph::g_union(white_attack, black_attack);
+
+        Graph white_position = Graph::g_union(white_support, white_attack);
+        Graph black_position = Graph::g_union(black_support, black_attack);
+        
 
         /*
         int scc_count = num_large_scc(tarjans(union_support), 1);
@@ -67,7 +107,7 @@ int main() {
         draw_edges(black_support, output, 240);
         draw_edges(white_attack, output, 60); // Red white attack edges
         draw_edges(black_attack, output, 180);
-        outfile = "../output/position" + std::to_string(i) + ".png"; 
+        outfile = outfolder + "position" + std::to_string(i) + ".png"; 
         output.writeToFile(outfile);
 
 
